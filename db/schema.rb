3ea4f66067cec6b968195adf085af0a8ad353a5d
ckpvthuong <ckpvthuongft@gmail.com>
2020-08-14 16:04:31 +0700

    search-view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_045313) do
+ActiveRecord::Schema.define(version: 2020_08_14_063801) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
