263d0dcd4672cc6d418a35353fbbd2ce74a212b4
ckpvthuong <ckpvthuongft@gmail.com>
2020-08-27 17:58:14 +0700

    fix-somthing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,6 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = 'wss://little-fish-learn-rails.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = 
-  [ 'https://little-fish-learn-rails.herokuapp.com',/http:\/\/little-fish-learn-rails.herokuapp.com.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
