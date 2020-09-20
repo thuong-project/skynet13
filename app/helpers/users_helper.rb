3781bf3668a076cba26927e79c9ae7f88a05eac1
thuong pham <ckpvthuongft@gmail.com>
2020-09-20 12:18:05 +0700

    fix-UI

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module UsersHelper
+    def active_button(path)
+        return request.original_fullpath == path ||
+               request.original_fullpath.include?(path + '?')
+    end
 end
