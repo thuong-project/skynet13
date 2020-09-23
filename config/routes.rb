302fa26c4d6d08bd974abe5518770be65b7eb54f
thuong pham <ckpvthuongft@gmail.com>
2020-09-23 08:46:16 +0700

    update-rootpath

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
-    root 'users#newsfeed'
+    root '/newsfeed'
 
     resources :users do
       get 'search', action: :search, on: :collection
